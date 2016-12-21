7e35774f153dccadeb03afdc2cb88f3f649c9c57
GlavniiLeopold <glavnii_leopold@mail.ru>
2016-12-21 15:44:00 +0300

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,16 @@
+parser.exe t00.txt
+parser.exe t01.txt
+parser.exe t02.txt
+parser.exe t03.txt
+parser.exe t04.txt
+parser.exe t05.txt
+parser.exe t06.txt
+parser.exe t07.txt
+parser.exe t08.txt
+parser.exe t09.txt
+parser.exe t10.txt
+parser.exe t11.txt
+parser.exe t12.txt
+parser.exe t13.txt
+parser.exe t14.txt
+pause
